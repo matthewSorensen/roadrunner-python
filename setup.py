e38469b0b34be1b9c24428c5b36b0c6e931261cf
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2012-12-16 21:50:48 +0000

    Setup proper python packaging.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='RoadRunner',
+    version='0.0dev',
+    packages=['roadrunnerlib'],
+    license='Apache License 2.0',
+    long_description=open('README.md').read(),
+)
