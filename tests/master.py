c451752f95bdd6298a6cf55efe79b2ee098f7ef2
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2012-12-19 23:25:54 +0000

    Increase test coverage
    As of now, tests/master.py covers 76% of the functions in rrPython
    and has the same approximate coverage as the old version. All functions
    that are deliberately ignored either segfault or are annoying (enableLogging),
    and the other functions either aren't tested by the old version or
    had blatantly wrong tests.

--- tests/master.py
+++ tests/master.py
@@ -17,6 +17,7 @@ class TestNoFailure(unittest.TestCase):
 
     def setUp(self):
          rrPython.loadSBMLFromFile(resource_filename('roadrunnerlib','data/feedback.xml'))
+
        
     def test_information(self):
         """ Tests that all of the methods returning static
@@ -36,7 +37,16 @@ class TestNoFailure(unittest.TestCase):
         model = rrPython.getSBML()
         self.assertIsNotNone(model)
         rrPython.loadSBML(model)
-   
+    def test_things_that_need_simulate(self):
+        covers(['simulate','getReactionRatesEx','getRatesOfChangeEx',
+                'getResultColumnLabel','getMatrixNumRows'])
+
+        rrPython.simulate()
+        rrPython.getReactionRatesEx([1.0,.9,.8,.6])
+        rrPython.getRatesOfChangeEx([1.0,.9,.8,.6])
+        rrPython.getResultColumnLabel()
+        rrPython.getMatrixNumRows()
+        
 # Adding somewhat meaningful tests for everything else is future work.
 # In the meantime, all other functions are simply executed - not throwing
 # an exception or segfaulting is taken as success.
@@ -86,12 +96,21 @@ test('setTimeEnd',5.0)
 test('setCapabilities','aaa')
 test('setTempFolder','.')
 test('setBoundarySpeciesByIndex',0,1.0)
-test('setGlobalSpeciesByIndex',0,1.0)
 test('setFloatingSpeciesByIndex',0,1.0)
 test('setComputeAndAssignConservationLaws','1')
 test('getBoundarySpeciesByIndex',0)
 test('getGlobalParameterByIndex',0)
-
+test('getReactionRate',0)
+test('getRateOfChange',0)
+test('getValue','S1')
+test('listToString',['a','b'])
+test('createVector',10)
+test('setLogLevel','DEBUG4')
+test('setTimeCourseSelectionList','time,S1,S')
+test('getCompartmentByIndex',0)
+test('getResultElement')
+test('setGlobalParameterByIndex',0,1.0)
+test('getFloatingSpeciesByIndex',0)
 
 if __name__ == '__main__':
     # Most of the time we don't want the list of functions that weren't covered or
@@ -105,6 +124,6 @@ if __name__ == '__main__':
         unseen = set(functions.keys()) - covers([]) - ignore
         f = len(functions)
         print 'Covered ',  round(100 * (f - len(unseen)) / f), '% of module functions'
-        if '--untested' in sys.argv:
+        if display_untested:
             for function in unseen:
                 print function, ' is untested'
